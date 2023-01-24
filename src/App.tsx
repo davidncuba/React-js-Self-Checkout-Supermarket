f3dfa00129e99c114b8b2a7d0c98f9a613d6de04
david cuba <dncuba@gmail.com>
2023-01-24 12:04:47 +0000

    configuration mui and react helmet async

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Page from "./components/Page";
+import { Container } from "@mui/material";
+
 function App() {
-  return <></>;
+  return (
+    <Page title="Main">
+      <Container maxWidth="lg">
+        <>Component</>
+      </Container>
+    </Page>
+  );
 }
 
 export default App;
---
+++ src/components/Page.tsx
@@ -0,0 +1,25 @@
+import { Box } from "@mui/material";
+import { forwardRef, ReactNode } from "react";
+import { Helmet } from "react-helmet-async";
+
+interface Props {
+  children: ReactNode;
+  meta?: ReactNode;
+  title: string;
+}
+
+const Page = forwardRef<HTMLDivElement, Props>(
+  ({ children, title = "", meta, ...other }, ref) => (
+    <>
+      <Helmet>
+        <title>{`${title} | Checkout`}</title>
+        {meta}
+      </Helmet>
+      <Box ref={ref} {...other}>
+        {children}
+      </Box>
+    </>
+  )
+);
+
+export default Page;
